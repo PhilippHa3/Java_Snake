4ef949787b0d503a3d800a42b1dfd55650d19f83
Philipp Haas <philipp.haas.er@gmail.com>
2025-12-04 20:39:40 +0100

    implementation Gym Env using Java Snake as backend

---
+++ RL_Agent/SnakeEnviroment.py
@@ -0,0 +1,60 @@
+import gymnasium as gym
+from gymnasium import spaces
+import socket, time
+import numpy as np
+
+class JavaSnakeEnv(gym.Env):
+    
+    def __init__(self, host='localhost', port=9000, board_size=12):
+        super(JavaSnakeEnv, self).__init__()
+
+        self.action_space = spaces.Discrete(4)
+
+        self.observation_space = spaces.Box(
+            low=0, high=3, shape=(board_size, board_size), dtype=np.int32
+        )
+
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.host, self.port))
+
+    def _parseInput(self, input_str:str):
+        obs_str, reward_str, done_str = input_str.strip().split('|')
+
+        obs_list = [[int(x) for x in row.split(',')] for row in obs_str.split(';')]
+        observation = np.array(obs_list, dtype=np.int32)
+
+        reward = float(reward_str)
+        done = done_str.lower() == 'true'
+
+        return observation, reward, done, {}
+
+    def step(self, action):
+        self.sock.sendall(f"{action}\n".encode('utf-8'))
+
+        data = self.sock.recv(4096).decode('utf-8')
+        obs, reward, done, info = self._parseInput(data)
+        return obs, reward, False, done, info
+
+    def reset(self, seed=None, options=None):
+        self.sock.sendall("-1\n".encode('utf-8'))
+        data = self.sock.recv(4096).decode('utf-8')
+
+        obs, _, _, info = self._parseInput(data)
+        return obs, info
+    
+    def close(self):
+        self.sock.close()
+
+
+if __name__ == '__main__':
+    env = JavaSnakeEnv(board_size=7)
+    running = True
+    obs, _ = env.reset()
+    i = 0
+    while running:
+        obs, rew, _, done, _ = env.step(i%4)
+        print(obs, '\n', rew, '\n', done)
+        time.sleep(1)
+        i += 1
