dec7374c6e483658178812ec34763ee72892bb72
Philipp Haas <philipp.haas.er@gmail.com>
2025-12-04 16:41:20 +0100

    made basic CLI UI

--- src/GUI.java
+++ src/GUI.java
@@ -1,20 +1,62 @@
-import javax.swing.*;
+import java.util.Scanner;
 
 public class GUI {
-    private static final int FIELD_WIDTH = 400;
-    private static final int FIELD_HEIGHT = 300;
+    public SnakeGame currentGame;
+    Scanner scanner;
 
-    private SnakeGame.GameState currentGameState;
+    public GUI(SnakeGame snakeGame) {
+        this.currentGame = snakeGame;
+        this.scanner = new Scanner(System.in);
+    }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Java Swing Game");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.setVisible(true);
+        SnakeGame snakeGame = new SnakeGame(5,5);
+        GUI gui = new GUI(snakeGame);
+        boolean running = true;
+        while (running){
+            gui.drawGameState();
+            String directionStr = gui.scanner.nextLine();
+            SnakeGame.Direction direction = null;
+            switch (directionStr) {
+                case "a":
+                    direction = SnakeGame.Direction.LEFT;
+                    break;
+                case "w":
+                    direction = SnakeGame.Direction.UP;
+                    break;
+                case "s":
+                    direction = SnakeGame.Direction.DOWN;
+                    break;
+                case "d":
+                    direction = SnakeGame.Direction.RIGHT;
+                    break;
+                default:
+                    break;
+            }
+            running = gui.currentGame.update(direction);
+        }
     }
 
-    public void updateGameState(SnakeGame.GameState newGS) {
-        this.currentGameState = newGS;
-    }
+    public void drawGameState() {
+        SnakeGame.GameState curGameState = this.currentGame.getGameState();
+        int height = curGameState.field_height;
+        int width = curGameState.field_width;
+        int[][] collision_array = curGameState.field_grid;
+        SnakeGame.Position food = curGameState.food;
 
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i == food.y && j == food.x) {
+                    System.out.print("x");
+                }
+                else if (collision_array[j][i] != 0) {
+                    System.out.print("o");
+                }
+                else {
+                    System.out.print("_");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
