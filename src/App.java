31ed38f07d509a99bae9238720d02d3f01a2c5a5
Philipp Haas <philipp.haas.er@gmail.com>
2025-12-04 20:36:18 +0100

    updated app to either start env backend or play in cl

--- src/App.java
+++ src/App.java
@@ -1,12 +1,25 @@
 public class App {
 
     public static void main(String[] args) throws Exception {
-        SnakeGame game = new SnakeGame(11, 11);
-        Thread inputThread = new Thread(new InputHandler(game));
-        GameEngine gameEngine = new GameEngine(game);
-        gameEngine.start();
-        inputThread.setDaemon(true);
-        inputThread.start();
+        boolean play = false;
+        if (play) {
+            SnakeGame game = new SnakeGame(9,9);
+            Thread inputThread = new Thread(new InputHandler(game));
+            GameEngine gameEngine = new GameEngine(game);
+            gameEngine.start();
+            inputThread.setDaemon(true);
+            inputThread.start();
+        }
+        else {
+            SnakeGame game = new SnakeGame(9,9);
+            GameEnvironment env = new GameEnvironment(game);
+    
+            try {
+                env.start(9000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
