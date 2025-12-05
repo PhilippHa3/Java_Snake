ee3ee6980a7b4412faaa3e16244c8b06b0a4fdb4
Philipp Haas <philipp.haas.er@gmail.com>
2025-12-05 10:28:12 +0100

    parameter updates PPO

--- RL_Agent/PPO_Agent.py
+++ RL_Agent/PPO_Agent.py
@@ -1,6 +1,7 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from SnakeEnviroment import JavaSnakeEnv
+import os
 
 if __name__ == '__main__':
     env = JavaSnakeEnv(board_size=9)
@@ -15,10 +16,10 @@ if __name__ == '__main__':
         gamma=0.99,
         verbose=1,
         seed=42,
-        tensorboard_log='C:/root/code_repositories/private_projects/Java_Snake/RL_Agent/logs/snake_ppo/'
+        tensorboard_log=f'{os.getcwd()}/RL_Agent/logs/snake_ppo/'
     )
 
     print('Training Started!')
-    model.learn(100_000)
+    model.learn(1_000_000)
     model.save('ppo_snake_agent')
     print('Traingin complete')
